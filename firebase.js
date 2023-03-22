be21a1bb581225e6b0cda6940446bb0944d875cf
daniarabykeev <daniarabykeev@gmail.com>
2023-03-22 19:58:17 +0600

    fix: change db.json

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWnCIvbSGZeveZa7q7hMBI69CPgpCbJfI",
+  authDomain: "pet-project-5a6c4.firebaseapp.com",
+  projectId: "pet-project-5a6c4",
+  storageBucket: "pet-project-5a6c4.appspot.com",
+  messagingSenderId: "770851152233",
+  appId: "1:770851152233:web:f340d22f1aa4a5d9835df8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
